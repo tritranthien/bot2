c32e9da2c67aa3ad7ada6c4d742126e752950399
Tri Tran <tri.tranthien@hdwebsoft.dev>
2025-03-21 09:01:43 +0700

    tesst

--- index.js
+++ index.js
@@ -208,7 +208,7 @@ async function sendHelpMessage(message) {
 \`${config.prefix}campuchia @user\` - Chích điện người dùng
 \`${config.prefix}ai [content]\` - Gọi AI
 \`${config.prefix}clear\` - Xóa tất cả tin nhắn của bot
-\`${config.prefix}test5\` - test
+\`${config.prefix}test\` - tôi vẫn còn sống.
 `;
   message.channel.send(helpText);
 }
