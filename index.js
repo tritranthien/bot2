e24e3c6c8e4534f802f83253a4a291d4366492f0
Tri Tran <tri.tranthien@hdwebsoft.dev>
2025-03-21 10:26:35 +0700

    test auto deploy

--- index.js
+++ index.js
@@ -137,7 +137,7 @@ client.on('messageCreate', async message => {
     case 'campuchia':
       campuchia(message);
       break;
-    case 'test5':
+    case 'test':
       message.reply('tôi còn sống!');
       break;
     case 'ai':
@@ -214,7 +214,7 @@ async function sendHelpMessage(message) {
 \`${config.prefix}campuchia @user\` - Chích điện người dùng
 \`${config.prefix}ai [content]\` - Gọi AI
 \`${config.prefix}clear\` - Xóa tất cả tin nhắn của bot
-\`${config.prefix}test9\` - tôi vẫn còn sống.
+\`${config.prefix}test\` - tôi vẫn còn sống.
 `;
   message.channel.send(helpText);
 }
