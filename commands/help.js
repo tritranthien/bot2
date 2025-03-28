e8741c54487c5ea30dd4e9a07ca652cc2b74f1be
Tan Luu <luuthanhtan0812@gmail.com>
2025-03-28 11:47:47 +0700

    show server

--- commands/help.js
+++ commands/help.js
@@ -7,7 +7,7 @@ module.exports = {
     description: 'Xem danh sách lệnh quản lý server. 📚',
     async execute(message, args, config) {
         const commandFiles = fs.readdirSync(path.resolve(__dirname)).filter(file => file.endsWith('.js')  && !file.startsWith('_') && file !== 'help.js');
-        let helpText = `**Lệnh Quản Lý Server 📚 ${process.env.APP_ENV1 ?? ''}**\n`;
+        let helpText = `**Lệnh Quản Lý Server 📚 ${process.env.APP_ENV ?? ''}**\n`;
 
         for (const file of commandFiles) {
             const command = require(path.resolve(__dirname, file));
