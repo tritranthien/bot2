832c57809fa1fc08c1ee7b0fbc27c97564b564a1
Tan Luu <luuthanhtan0812@gmail.com>
2025-06-20 15:37:31 +0700

    job

--- src/queues/agendaQueue.ts
+++ src/queues/agendaQueue.ts
@@ -1,15 +1,17 @@
 import { agenda } from '../../utils/agenda.js';
 
 const SEND_HOURS = [8, 9, 10, 12, 14, 16, 18];
-
 const toUtcHour = (vnHour: number) => (vnHour + 24 - 7) % 24;
 
 export const scheduleDailyJobs = async () => {
   for (const vnHour of SEND_HOURS) {
     const utcHour = toUtcHour(vnHour);
-    await agenda.every(`0 ${utcHour} * * 1-5`, `send scheduled message at ${vnHour}`, {
-      hour: vnHour,
-    });
+    await agenda.every(
+      `0 ${utcHour} * * 1-5`,
+      `send scheduled message at ${vnHour}`,
+      { hour: vnHour },
+      { cron: true } as any
+    );
   }
   console.log('📆 Đã lên lịch gửi tin nhắn theo giờ Việt Nam bằng Agenda.');
 };
